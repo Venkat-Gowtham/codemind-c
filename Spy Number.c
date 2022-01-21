b67b880413782080ef2691e7c2fb6d46f4b54578
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-01-21 16:44:09 +0530

    #include<stdio.h> int main() {     int n,r,sum=0,pro=1;      scanf("%d",&n);      while (n>0)      {          r=n%10;          n=n/10;          sum += r;          pro *= r;      }      if(sum==pro)         {            printf("Spy Number");
            }
         else
             { 
                 printf("Not Spy Number");
             }
    }

---
+++ Spy Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,sum=0,pro=1;
+     scanf("%d",&n);
+     while (n>0)
+     {
+         r=n%10;
+         n=n/10;
+         sum += r;
+         pro *= r;
+     }
+     if(sum==pro)
+        {
+           printf("Spy Number");
+     
+        }
+     else
+         { 
+             printf("Not Spy Number");
+         }
+}
+      
+         
