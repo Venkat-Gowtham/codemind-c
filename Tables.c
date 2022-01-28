e30e523df43ff7bb070843222d8b6fdac72f9791
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-01-28 16:22:35 +0530

    #include<stdio.h> int main() {     int n,i,l,t;     scanf("%d%d",&n,&l);     if(n,l>=1 && n,l<=1000)     {         for(i=1;i<=l;i++)          {              t=n*i;              if(i%2!=0)              {                 printf("%d x %d = %d ",n,i,t);              }          }     } }

---
+++ Tables.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,l,t;
+    scanf("%d%d",&n,&l);
+    if(n,l>=1 && n,l<=1000)
+    {
+        for(i=1;i<=l;i++)
+         {  
+             t=n*i;
+             if(i%2!=0)
+             {
+                printf("%d x %d = %d
+",n,i,t);
+             }
+         }
+    }
+}
