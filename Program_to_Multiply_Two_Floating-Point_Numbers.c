e7f26beafee8a610bcfc8ae160a013934816737e
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-02-11 10:15:13 +0530

    //functions// #include<stdio.h> float product(float a,float b) { 	float pro=a*b; 	return pro; } int main () { 	float x,y; 	scanf("%f%f",&x,&y); 	printf("%.2f",product(x,y)); }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.c
@@ -0,0 +1,13 @@
+//functions//
+#include<stdio.h>
+float product(float a,float b)
+{
+	float pro=a*b;
+	return pro;
+}
+int main ()
+{
+	float x,y;
+	scanf("%f%f",&x,&y);
+	printf("%.2f",product(x,y));
+}
