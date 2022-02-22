52db7db4bda5bcea6244c311352c8a85e943af46
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-02-22 23:27:38 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     while(n>0)     {     for(i=1;i<=n;i++)     {         printf("%d",i);
        }
        printf("
    ");
        n--;
        }
    }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    while(n>0)
+    {
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i);
+        
+    }
+    printf("
+");
+    n--;
+    }
+}
