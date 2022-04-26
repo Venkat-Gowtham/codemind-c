1b31dd786445510c476d99b0915872124a6a79c0
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-04-26 20:07:14 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,a[n],sum=0,v=0,res=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             sum+=a[i];         }     }     printf("%d",sum);
    }

---
+++ ODD_Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,a[n],sum=0,v=0,res=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            sum+=a[i];
+        }
+    }
+    printf("%d",sum);
+ 
+}
