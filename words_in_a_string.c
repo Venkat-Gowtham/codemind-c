aeb5ed8a8af3c7ff31ad63673e8d842beea6d3b3
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-05-02 19:44:10 +0530

    #include <stdio.h> #include <string.h> int main() {     char s[200];     int count = 0,i,c=0;     scanf("%[^ ]s", s);  for(i=0;s[i]!=NULL;i++)     {         if (s[i]==' ')         {             count++;         }     }     printf("%d",count+1); }

---
+++ words_in_a_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[200];
+    int count = 0,i,c=0;
+    scanf("%[^
+]s", s);
+ for(i=0;s[i]!=NULL;i++)
+    {
+        if (s[i]==' ')
+        {
+            count++;
+        }
+    }
+    printf("%d",count+1);
+}
