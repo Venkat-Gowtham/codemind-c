a210b18dba45babacbb4c618ec2d581618cc3a8d
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-05-02 14:38:41 +0530

    #include<stdio.h> int main() {     int m,n;     scanf("%d%d",&m,&n);     int a[n][n],sum=0,i,j,res=0,diff=0,v;     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<m;i++)     { v=0;         for(j=0;j<n;j++)         {            v+=a[i][j];         }         if(v>diff)         {             diff=v;         }     }        printf("%d",diff); }

---
+++ Maximum_sum_of_row_wise_sum_of_a_matrix.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d%d",&m,&n);
+    int a[n][n],sum=0,i,j,res=0,diff=0,v;
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<m;i++)
+    { v=0;
+        for(j=0;j<n;j++)
+        {
+           v+=a[i][j];
+        }
+        if(v>diff)
+        {
+            diff=v;
+        }
+    }
+       printf("%d",diff);
+}
