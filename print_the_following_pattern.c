1ef7a6027b57be13727f683569cd9411d051416f
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-03 21:07:25 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if (j==1 || i==j || i==n)             {                 printf("*");             }             else             {             printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,24 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,k;
+    int n,i,j;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i-1;j++)
+        for(j=1;j<=n;j++)
         {
-            printf(" ");
-        }
-        for(k=1;k<=n;k++)
-        {
-            if(i==1||i==n ||k==1||k==n)
+            if (j==1 || i==j || i==n)
             {
                 printf("*");
             }
             else
-             {
-                 printf(" ");
-             }
+            {
+            printf(" ");
+            }
         }
         printf("
 ");
