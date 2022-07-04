32d14ecaffb666f2d78fc58a92929f4f2281306f
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-04 20:10:36 +0530

    #include<stdio.h> int main() {     int m,n,a[10][10],i,j,b[10][10];     scanf("%d",&m);     for(i=0;i<m;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<m;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&b[i][j]);         }     }     int s=0,k=0;     for(i=0;i<m;i++)     {         for(j=0;j<m;j++)         {             if(a[i][j]>b[i][j])             {                 if(j+1<m)                 {                 printf("%d ",a[i][j]-b[i][j]);                 }                 else                 {                     printf("%d",a[i][j]-b[i][j]);                 }             }             else             {                 if(j+1<m)                 {                 printf("%d ",b[i][j]-a[i][j]);                 }                 else{                     printf("%d",b[i][j]-a[i][j]);                 }             }         }         printf(" ");     }
    }

--- Absolute_difference_of_two_matrices.c
+++ Absolute_difference_of_two_matrices.c
@@ -1,54 +1,51 @@
 #include<stdio.h>
 int main()
 {
-    int m,n;
-    scanf("%d",&n);
-    int a[n][n],b[n][n],c[n][n],sum=0,diff=0,i,j;
-    for(i=0;i<n;i++)
+    int m,n,a[10][10],i,j,b[10][10];
+    scanf("%d",&m);
+    for(i=0;i<m;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<m;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<m;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<m;j++)
         {
             scanf("%d",&b[i][j]);
         }
     }
-    for(i=0;i<n;i++)
+    int s=0,k=0;
+    for(i=0;i<m;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<m;j++)
         {
-           if(a[i][j]>=b[i][j])
-           {
-               c[i][j]=a[i][j]-b[i][j];
-           }
-           if(a[i][j]<b[i][j])
-           {
-               c[i][j]=b[i][j]-a[i][j];
-           }
-        }
-        
-    }
-     for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            if(j==n-1)
+            if(a[i][j]>b[i][j])
             {
-                printf("%d",c[i][j]);
+                if(j+1<m)
+                {
+                printf("%d ",a[i][j]-b[i][j]);
+                }
+                else
+                {
+                    printf("%d",a[i][j]-b[i][j]);
+                }
             }
             else
             {
-                printf("%d ",c[i][j]);
-              }
+                if(j+1<m)
+                {
+                printf("%d ",b[i][j]-a[i][j]);
+                }
+                else{
+                    printf("%d",b[i][j]-a[i][j]);
+                }
+            }
         }
         printf("
 ");
     }
-    
     
 }
