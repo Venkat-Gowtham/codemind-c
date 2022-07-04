c9f38d74d6dd9f65a8b70408d83a9c93dc4f795b
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-04 19:25:21 +0530

    #include<stdio.h> int main() {     int m,n,a[10][10],i,j;     scanf("%d%d",&m,&n);     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);         }     }     int s=0,k=0;     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             s+=a[i][j];         }     }     printf("%d",s);
    }

--- Sum_of_all_elements_in_a_matrix.c
+++ Sum_of_all_elements_in_a_matrix.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int m,n;
+    int m,n,a[10][10],i,j;
     scanf("%d%d",&m,&n);
-    int a[m][n],sum=0,diff=0,i,j;
     for(i=0;i<m;i++)
     {
         for(j=0;j<n;j++)
@@ -11,13 +10,14 @@ int main()
             scanf("%d",&a[i][j]);
         }
     }
+    int s=0,k=0;
     for(i=0;i<m;i++)
     {
         for(j=0;j<n;j++)
         {
-           diff+=a[i][j];
+            s+=a[i][j];
         }
     }
-    printf("%d",diff);
+    printf("%d",s);
     
 }
